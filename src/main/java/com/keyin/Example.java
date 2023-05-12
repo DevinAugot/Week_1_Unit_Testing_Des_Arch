a83d749d8536db3360194ae25f7a8159ff7e078f
DevinAugot <105358840+DevinAugot@users.noreply.github.com>
2023-05-12 10:05:23 -0230

    Initial commit

---
+++ src/main/java/com/keyin/Example.java
@@ -0,0 +1,11 @@
+package com.keyin;
+
+public class Example {
+
+    public Integer multiply(int x, int y) {
+        return x * y;
+    }
+    public String format(String stringToFormat){
+       return stringToFormat.trim().replaceAll(" ",",");
+    }
+}
